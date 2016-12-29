dc27650111ee655edd7409344d4196f6e0f9ef87
Hanson Li <lihsca@gmail.com>
2016-12-29 13:53:12 -0500

    codelines

--- codelines.go
+++ codelines.go
@@ -10,11 +10,11 @@ type CodeLines struct {
 }
 
 func NewCodeLines() *CodeLines {
-    return &CodeLines{make([]string, 0)}
+    return &CodeLines{lines: make([]string, 0)}
 }
 
-func (c *CodeLines) Push(s string) {
-    c.lines = append(c.lines, s)
+func (c *CodeLines) Push(s string, a...interface{}) {
+    c.lines = append(c.lines, fmt.Sprintf(s, a...))
 }
 
 func (c *CodeLines) ToString() string {
@@ -27,6 +27,7 @@ func main() {
     lines.Push("<Request>")
     lines.Push("<Item>")
     lines.Push(strfmt("<Weight>%d</Weight>", 23))
+    lines.Push("<Height>%d</Height>", 45)
     lines.Push("</Item>")
     lines.Push("</Request>")
     fmt.Println(lines.ToString())
